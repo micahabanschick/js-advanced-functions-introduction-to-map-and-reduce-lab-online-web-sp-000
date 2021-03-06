3e7b6eea751eb6e53f815b102ff1dd2bb8038df7
Micah Banschick <micha.ban@gmail.com>
2021-03-06 01:13:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Your code here
 const mapToNegativize = (sourceArray) => {
-    const negArray = []; 
+    const negArray = [];
     for(let x of sourceArray){
-        negArray.push(x = x * -1) 
+        negArray.push(x = x * -1)
     }
-    return negArray 
+    return negArray
 }
 
 const mapToNoChange = (sourceArray) => {
@@ -12,45 +12,45 @@ const mapToNoChange = (sourceArray) => {
 }
 
 const mapToDouble = (sourceArray) => {
-    const doubleArray = []; 
+    const doubleArray = [];
     for(let x of sourceArray){
-        doubleArray.push(x = x * 2) 
+        doubleArray.push(x = x * 2)
     }
-    return doubleArray 
+    return doubleArray
 }
 
 const mapToSquare = (sourceArray) => {
-    const squareArray = []; 
+    const squareArray = [];
     for(let x of sourceArray){
-        squareArray.push(x = x**2) 
+        squareArray.push(x = x**2)
     }
-    return squareArray 
+    return squareArray
 }
 
 const reduceToTotal = (sourceArray, startingPoint=0) => {
     let total = startingPoint
     for(let num of sourceArray){
-        total = total + num 
+        total = total + num
     }
     return total
 }
 
 const reduceToAllTrue = (sourceArray) => {
-    let value = true 
+    let value = true
     for(let item of sourceArray){
         if (item === false){
-            value = false 
+            value = false
         }
     }
-    return value 
+    return value
 }
 
 const reduceToAnyTrue = (sourceArray) => {
-    let value = false 
+    let value = false
     for(let item of sourceArray){
         if (item === true){
-            value = true 
+            value = true
         }
     }
-    return value 
+    return value
 } 
